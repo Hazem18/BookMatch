6a0d403459d118151765983cd6fc5837496833d8
abdoelsayedDragon1 <abdo.com.a777@gmail.com>
2024-12-04 00:00:01 +0200

    edit CRUD League and Add CRUD TicketCategory

---
+++ BookMatch/Areas/Admin/Controllers/TicketCategory.csController.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMatch.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TicketCategory : Controller
+    {
+        private readonly ITicketCategoryRepository ticketCategoryRepository;
+
+        public TicketCategory(ITicketCategoryRepository ticketCategoryRepository)
+        {
+            this.ticketCategoryRepository = ticketCategoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var ticet = ticketCategoryRepository.Get();
+            return View(model:ticet);
+        }
+    }
+}
