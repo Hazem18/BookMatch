a46b9bf3c54379f1f2ca3caa3029e1c0409a6c4a
Hazem Emad <hazem.emad344@gmail.com>
2024-12-03 20:31:20 +0200

    Read action for the Team model

--- DataAccess/Repository/TeamRepository.cs
+++ DataAccess/Repository/TeamRepository.cs
@@ -1,8 +1,10 @@
 ﻿using DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
 using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +13,44 @@ namespace DataAccess.Repository
     public class TeamRepository : Repository<Team>, ITeamRepository
     {
         private readonly ApplicationDbContext dbContext;
-
+        private DbSet<Team> dbSet;
         public TeamRepository(ApplicationDbContext dbContext) : base(dbContext)
         {
             this.dbContext=dbContext;
+            dbSet = dbContext.Set<Team>();
         }
+
+        public IEnumerable<Team> GetTeams(Expression<Func<Team, bool>>? filter = null,bool includeStadium = false,bool includeLeagues = false,bool tracked = true)
+        {
+            IQueryable<Team> query = dbSet;
+
+            // Apply filter if provided
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            // Include Stadium navigation property if requested
+            if (includeStadium)
+            {
+                query = query.Include(t => t.Stadium);
+            }
+
+            // Include TeamLeagues and Leagues if requested
+            if (includeLeagues)
+            {
+                query = query.Include(t => t.TeamLeagues)
+                             .ThenInclude(tl => tl.League);
+            }
+
+            // Apply tracking or no-tracking
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+
+            return query.ToList();
+        }
+
     }
 }
