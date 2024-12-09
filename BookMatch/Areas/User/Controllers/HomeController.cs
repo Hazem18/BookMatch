8deb1c88a83495218f9d4ad95bd96a122f92f5ef
M0uradMohamed <mouradmoahmed@gmail.com>
2024-12-10 01:01:51 +0200

    made details action and view but not full completed

--- BookMatch/Areas/User/Controllers/HomeController.cs
+++ BookMatch/Areas/User/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using System.Diagnostics;
@@ -8,17 +9,44 @@ namespace BookMatch.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ITicketRepository ticketRepository;
+        private readonly IMatchRepository matchRepository;
+        private readonly ITicketCategoryRepository ticketCategoryRepository;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        public HomeController(ILogger<HomeController> logger, ITicketRepository ticketRepository,
+            IMatchRepository matchRepository , ITicketCategoryRepository ticketCategoryRepository)
+        { 
             _logger = logger;
+            this.ticketRepository = ticketRepository;
+            this.matchRepository = matchRepository;
+            this.ticketCategoryRepository = ticketCategoryRepository;
         }
 
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Notfound()
+        public IActionResult Details(int id) 
+        {
+            var match = matchRepository.GetOne(expression: e=>e.Id == id ,
+                includeProps: [e=>e.Stadium , e => e.TeamA , e => e.TeamB , e => e.League ]);
+            if (match == null)
+            {
+                return RedirectToAction ("Notfound");  
+            }
+            else
+            {
+                var ticketcategories = ticketCategoryRepository.Get();
+                ViewBag.TicketCategories = ticketcategories;
+                return View(match);
+            }
+        }
+        [HttpPost]
+        public IActionResult Details(int id, int ticketCategoryId , string seatNumber)
+        {
+            return View();
+        }
+            public IActionResult Notfound()
         {
 
             return View();
